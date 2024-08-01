c85c0daac859169feae7a764d22dccf941c6f008
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-07-31 21:44:00 -0400

    lexer optimization

---
+++ src/lexer/cursor/en.rs
@@ -0,0 +1,97 @@
+use crate::error::LexingError;
+use crate::lexer::*;
+use crate::lexer::cursor::Cursor;
+
+impl Cursor<'_> {
+    pub fn en(&mut self) -> Option<Result<Token, LexingError>> {
+        if let Some(first) = self.chars.next() {
+            let token = match first {
+                '0'..='9' => self.number(first),
+                '+' | '-' | '*' | '/' | '%' => self.arithmetic(first),
+                '\'' | '"' => self.en_string(first),
+                'a'..='z' | 'A'..='Z' | '_' => self.en_ident(first),
+                '>' | '<' | '=' | '!' => self.en_comparative(first),
+                '(' | ')' | '{' | '}' | '|' | ';' | ',' => self.en_symbol(first),
+                other => Err(LexingError::InvalidChar { c: other })
+            };
+            Some(token)
+        } else {
+            None
+        }
+    }
+
+    pub fn en_symbol(&mut self, first: char) -> Result<Token, LexingError> {
+        let tt = match first {
+            '(' => SymbolType::LParen.into(),
+            ')' => SymbolType::RParen.into(),
+            '{' => SymbolType::LBrace.into(),
+            '}' => SymbolType::RBrace.into(),
+            ';' => SymbolType::Semicol.into(),
+            '|' => SymbolType::Pipe.into(),
+            ',' => SymbolType::Comma.into(),
+            other => return Err(LexingError::InvalidChar { c: other })
+        };
+        Ok(Token::new(tt, first.to_string()))
+    }
+
+    pub fn en_ident(&mut self, first: char) -> Result<Token, LexingError> {
+        let mut value = String::from(first);
+        while let Some(ch) = self.chars.peek() {
+            if ch.is_ascii_alphanumeric() || *ch == '_' {
+                value.push(*ch);
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+        let tt = match value.as_str() {
+            "and" => BinoptrType::And.into(),
+            "xor" => BinoptrType::Xor.into(),
+            "or" => BinoptrType::Or.into(),
+            "if" => KeywordType::If.into(),
+            "elif" => KeywordType::Elif.into(),
+            "else" => KeywordType::Else.into(),
+            "while" => KeywordType::While.into(),
+            "return" => KeywordType::Return.into(),
+            "true" => ValueType::Boolean.into(),
+            "false" => ValueType::Boolean.into(),
+            "none" => ValueType::None.into(),
+            _ => TokenType::Identifier
+        };
+        Ok(Token::new(tt, value))
+    }
+
+    pub fn en_string(&mut self, first: char) -> Result<Token, LexingError> {
+        let mut value = String::new();
+        for ch in self.chars.by_ref() {
+            if ch == first {
+                return Ok(Token::new(ValueType::String.into(), value));
+            } else {
+                value.push(ch)
+            }
+        }
+        Err(LexingError::StringNotClosed { s: value })
+    }
+
+    pub fn en_comparative(&mut self, first: char) -> Result<Token, LexingError> {
+        let mut value = String::from(first);
+        if let Some(eq) = self.chars.peek() {
+            if *eq == '=' {
+                value.push('=');
+                self.chars.next();
+            }
+        }
+        let tt = match value.as_str() {
+            ">" => BinoptrType::Gt.into(),
+            "<" => BinoptrType::Lt.into(),
+            "=" => AssignType::Asn.into(),
+            "!" => UnaoptrType::Not.into(),
+            ">=" => BinoptrType::Ge.into(),
+            "<=" => BinoptrType::Le.into(),
+            "==" => BinoptrType::Eq.into(),
+            "!=" => BinoptrType::Ne.into(),
+            _ => return Err(LexingError::UnknownBinoptr { s: value })
+        };
+        Ok(Token::new(tt, value))
+    }
+}
--- src/lexer/cursor/shared.rs
+++ src/lexer/cursor/shared.rs
@@ -1,11 +1,11 @@
 use crate::error::LexingError;
-use crate::lexer::{AssignType, BinoptrType, SymbolType, Token, TokenType, UnaoptrType, ValueType};
+use crate::lexer::*;
 use crate::lexer::cursor::Cursor;
 
 impl Cursor<'_> {
-    pub fn skip_spaces(&mut self) {
+    pub fn skip_whitespace(&mut self) {
         while let Some(ch) = self.chars.peek() {
-            if *ch == ' ' || *ch == '\n' || *ch == '\r' {
+            if [' ', '\n', '\r'].contains(ch) {
                 self.chars.next();
             } else {
                 break;
@@ -13,120 +13,52 @@ impl Cursor<'_> {
         }
     }
 
-    pub fn scan_number(&mut self) -> Result<Token, LexingError> {
-        let mut value = String::new();
+    pub fn number(&mut self, first: char) -> Result<Token, LexingError> {
+        let mut value = String::from(first);
         let mut dotted = false;
         while let Some(ch) = self.chars.peek() {
-            if ch.is_ascii_digit() || *ch == '.' {
-                match (*ch, dotted) {
-                    ('.', true) => return Ok(Token::new(ValueType::Number.into(), value)),
-                    ('.', false) => dotted = true,
-                    _ => ()
-                }
-                value.push(*ch);
-                self.chars.next();
-            } else {
-                break;
+            match (ch, dotted) {
+                ('.', true) => return Ok(Token::new(ValueType::Number.into(), value)),
+                ('.', false) => dotted = true,
+                ('0'..='9', _) => (),
+                _ => break
             }
+            value.push(*ch);
+            self.chars.next();
         }
-
-        if value.as_str() == "." {
-            Err(LexingError::NumberJustADot)
-        } else {
-            Ok(Token::new(ValueType::Number.into(), value))
-        }
+        Ok(Token::new(ValueType::Number.into(), value))
     }
 
-    pub fn scan_simple_binoptr(&mut self) -> Result<Token, LexingError> {
-        let mut value = String::new();
-
-        if let Some(ch) = self.chars.next() {
-            value.push(ch);
-        } else {
-            return Err(LexingError::EndOfCharStream);
-        }
-
+    pub fn arithmetic(&mut self, first: char) -> Result<Token, LexingError> {
+        let mut value = String::from(first);
         if let Some(eq) = self.chars.peek() {
             if *eq == '=' {
                 value.push('=');
                 self.chars.next();
             }
         }
-
-        let ttype = match value.as_str() {
-            "*" => BinoptrType::Mul.into(),
-            "/" => BinoptrType::Div.into(),
-            "%" => BinoptrType::Mod.into(),
-            "*=" => AssignType::Mue.into(),
-            "/=" => AssignType::Die.into(),
-            "%=" => AssignType::Moe.into(),
-            _ => return Err(LexingError::UnknownBinoptr { s: value })
-        };
-
-        Ok(Token::new(ttype, value))
-    }
-
-    pub fn scan_additive_optr(&mut self) -> Result<Token, LexingError> {
-        let mut value = String::new();
-
-        if let Some(ch) = self.chars.next() {
-            value.push(ch);
-        } else {
-            return Err(LexingError::EndOfCharStream);
-        }
-
-        if let Some(eq) = self.chars.peek() {
-            if *eq == '=' {
-                value.push('=');
-                self.chars.next();
-            }
-        }
-
-        let binary = matches!(self.buffer.last(),
-            Some(prev) if matches!(prev.kind,
+        let binary = matches!(
+            self.buffer.last(),
+            Some(prev) if matches!(
+                prev.kind,
                 TokenType::Val(_) |
                 TokenType::Identifier |
                 TokenType::Sym(SymbolType::RParen)
             )
         );
-
-        let tt = match (binary, value.as_str()) {
-            (true, "+") => BinoptrType::Add.into(),
-            (true, "-") => BinoptrType::Sub.into(),
-            (false, "+") => UnaoptrType::Pos.into(),
-            (false, "-") => UnaoptrType::Neg.into(),
-            (_, "+=") => AssignType::Ade.into(),
-            (_, "-=") => AssignType::Sue.into(),
-            _ => return Err(LexingError::UnknownBinoptr { s: value })
-        };
-        Ok(Token::new(tt, value))
-    }
-
-    pub fn scan_comparative_optr(&mut self) -> Result<Token, LexingError> {
-        let mut value = String::new();
-
-        if let Some(ch) = self.chars.next() {
-            value.push(ch);
-        } else {
-            return Err(LexingError::EndOfCharStream);
-        }
-
-        if let Some(eq) = self.chars.peek() {
-            if *eq == '=' {
-                value.push('=');
-                self.chars.next();
-            }
-        }
-
-        let tt = match value.as_str() {
-            ">" => BinoptrType::Gt.into(),
-            "<" => BinoptrType::Lt.into(),
-            "=" => AssignType::Asn.into(),
-            "!" => UnaoptrType::Not.into(),
-            ">=" => BinoptrType::Ge.into(),
-            "<=" => BinoptrType::Le.into(),
-            "==" => BinoptrType::Eq.into(),
-            "!=" => BinoptrType::Ne.into(),
+        let tt = match (value.as_str(), binary) {
+            ("+", true) => BinoptrType::Add.into(),
+            ("-", true) => BinoptrType::Sub.into(),
+            ("+", false) => UnaoptrType::Pos.into(),
+            ("-", false) => UnaoptrType::Neg.into(),
+            ("*", _) => BinoptrType::Mul.into(),
+            ("/", _) => BinoptrType::Div.into(),
+            ("%", _) => BinoptrType::Mod.into(),
+            ("+=", _) => AssignType::Ade.into(),
+            ("-=", _) => AssignType::Sue.into(),
+            ("*=", _) => AssignType::Mue.into(),
+            ("/=", _) => AssignType::Die.into(),
+            ("%=", _) => AssignType::Moe.into(),
             _ => return Err(LexingError::UnknownBinoptr { s: value })
         };
         Ok(Token::new(tt, value))
