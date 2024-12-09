94527b36b1b00a032e2255995dcde42ccecee643
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-12-09 12:47:03 +0800

    Update helper.rs

--- felys/packrat/helper/src/helper.rs
+++ felys/packrat/helper/src/helper.rs
@@ -44,26 +44,39 @@ pub fn lecursion_helper(body: TokenStream) -> TokenStream {
     let block = &body.block;
 
     quote!(
-        #[::packrat::memoize]
         #vis #sig {
             let pos = self.stream.cursor;
-            let none: #output = None;
-            let mut res = Self::CR::from(none);
-            let mut end = pos;
-            loop {
-                let mode = self.stream.strict;
-                let sig = stringify!(#sig);
-                self.memo.insert(pos, mode, sig, end, res.clone());
-                let result = || -> #output #block();
-                if end < self.stream.cursor {
-                    res = Self::CR::from(result);
-                    end = self.stream.cursor;
-                    self.stream.cursor = pos;
-                } else {
-                    self.stream.cursor = end;
-                    break res.into();
-                }
+            let mode = self.stream.strict;
+            let sig = stringify!(#sig);
+            if let Some(memo) = self.memo.get(pos, mode, sig) {
+                let (end, res) = memo;
+                self.stream.cursor = end;
+                return res.into()
             }
+            let result = || -> #output {
+                let pos = self.stream.cursor;
+                let none: #output = None;
+                let mut res = Self::CR::from(none);
+                let mut end = pos;
+                loop {
+                    let mode = self.stream.strict;
+                    let sig = stringify!(#sig);
+                    self.memo.insert(pos, mode, sig, end, res.clone());
+                    let result = || -> #output #block();
+                    if end < self.stream.cursor {
+                        res = Self::CR::from(result);
+                        end = self.stream.cursor;
+                        self.stream.cursor = pos;
+                    } else {
+                        self.stream.cursor = end;
+                        break res.into();
+                    }
+                }
+            }();
+            let end = self.stream.cursor;
+            let res = Self::CR::from(result.clone());
+            self.memo.insert(pos, mode, sig, end, res);
+            result
         }
     ).into()
 }
