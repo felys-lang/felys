422f3769e272f3e20d6b9bdf68e5730306f055dc
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2025-01-10 20:38:29 -0500

    Update main.rs

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -2,9 +2,9 @@ use felys::parse;
 use std::time::Instant;
 
 const PAYLOADS: [&str; 3] = [
-    "1 + (2 >= 4 != (5 <= 6 or 7)) == 8 - 9 and 10 + not (((11 * 12 / 13 % 14 - 15 * -16)));",
+    "1.0 + (0.1 >= 4 != (5 <= 6 or 7)) == 8 - 9 and 0 + not (((11 * 12 / 3 % 14 - 1 * -3)));",
     "if true { while true { loop { break; continue; }}} else if true { x = 10; } else false;",
-    "break (3 + 4 * (math.sin(2)*2)) / (5 - 2 * (if x > 10 { x } else y)) + 3 * 3 - +-+-+-3;"
+    "break (3 + 4 * (math.sin(2)*2)) / (5 - 2 * (if x > 10 { x } else y)) + 3 - -2 \"hello\";"
 ];
 
 fn main() {
