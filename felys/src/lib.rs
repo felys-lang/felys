7085c7967e0e0846279eacfdb8834e1881b839cc
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2025-06-11 17:21:48 -0400

    /

--- felys/src/lib.rs
+++ felys/src/lib.rs
@@ -1,2 +1,17 @@
 mod ast;
 mod rspegen;
+
+#[cfg(test)]
+mod tests {
+    use crate::rspegen::Packrat;
+
+    #[test]
+    fn playground() {
+        let mut packrat = Packrat::from("let x = a".to_string());
+        let result = packrat.grammar();
+        if let Some((loc, msg)) = packrat.snapshot {
+            println!("{} @ {}", msg, loc);
+        }
+        println!("{:?}", result);
+    }
+}
--- felys/src/rspegen/helper.rs
+++ felys/src/rspegen/helper.rs
@@ -1,6 +1,16 @@
 use crate::rspegen::Packrat;
 
 impl Packrat {
+    pub fn ident(&mut self) -> Option<usize> {
+        let id = self.IDENT()?;
+        let ident = self.intern.get(&id).unwrap();
+        if self.keywords.filter(ident).is_some() {
+            Some(id)
+        } else {
+            None
+        }
+    }
+    
     pub fn eof(&mut self) -> Option<()> {
         self.stream.trim();
         if self.stream.next().is_none() {
