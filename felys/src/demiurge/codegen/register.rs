cfc6e1378c951757ef88f2ba429f91df7a65fb82
FelysNeko <jhannykin16@gmail.com>
2026-01-17 20:38:21 -0500

    Update register.rs

--- felys/src/demiurge/codegen/register.rs
+++ felys/src/demiurge/codegen/register.rs
@@ -20,6 +20,24 @@ struct Context {
     uses: HashMap<Var, usize>,
 }
 
+impl Context {
+    fn define(&mut self, var: &Var, index: usize) {
+        self.defs.entry(*var).or_insert(index);
+    }
+
+    fn using(&mut self, var: &Var, index: usize) {
+        match self.uses.entry(*var) {
+            Entry::Occupied(mut e) => {
+                let last = max(index, *e.get());
+                e.insert(last);
+            }
+            Entry::Vacant(e) => {
+                e.insert(index);
+            }
+        };
+    }
+}
+
 impl Function {
     pub fn allocate(&self, copies: &HashMap<Label, Vec<Copy>>) -> (HashMap<Var, Reg>, usize) {
         let ctx = self.precompute(copies);
@@ -59,6 +77,8 @@ impl Function {
 
     fn precompute(&self, copies: &HashMap<Label, Vec<Copy>>) -> Context {
         let mut ctx = Context::default();
+        let mut anchors = HashMap::new();
+        let mut loops = Vec::new();
 
         let mut index = 0;
         for i in self.args.clone() {
@@ -66,22 +86,37 @@ impl Function {
             index += 1;
         }
         for label in self.rpo() {
+            anchors.insert(label, index);
             let fragment = self.get(label).unwrap();
             for (idx, instruction) in fragment.instructions.iter().enumerate() {
-                instruction.dn(index, &mut ctx);
+                instruction.du(index, &mut ctx);
                 ctx.indices.insert((label, Id::Ins(idx)), index);
                 index += 1;
             }
             if let Some(copy) = copies.get(&label) {
                 for (idx, copy) in copy.iter().enumerate() {
-                    copy.dn(index, &mut ctx);
+                    copy.du(index, &mut ctx);
                     ctx.indices.insert((label, Id::Copy(idx)), index);
                     index += 1;
                 }
             }
-            fragment.terminator.as_ref().unwrap().dn(index, &mut ctx);
+            fragment
+                .terminator
+                .as_ref()
+                .unwrap()
+                .du(index, &mut ctx, &mut anchors, &mut loops);
             ctx.indices.insert((label, Id::Term), index);
+
             index += 1;
+        }
+
+        for (start, end) in loops {
+            for (var, last) in ctx.uses.iter_mut() {
+                let def = ctx.defs[var];
+                if def < start && *last >= start {
+                    *last = max(*last, end);
+                }
+            }
         }
 
         ctx
@@ -89,89 +124,75 @@ impl Function {
 }
 
 impl Instruction {
-    fn dn(&self, index: usize, ctx: &mut Context) {
-        let mut update = |var: &Var| {
-            match ctx.uses.entry(*var) {
-                Entry::Occupied(mut e) => {
-                    let last = max(index, *e.get());
-                    e.insert(last);
-                }
-                Entry::Vacant(e) => {
-                    e.insert(index);
-                }
-            };
-        };
+    fn du(&self, index: usize, ctx: &mut Context) {
         match self {
             Instruction::Field(dst, src, _)
             | Instruction::Unpack(dst, src, _)
             | Instruction::Unary(dst, _, src) => {
-                ctx.defs.insert(*dst, index);
-                update(src)
+                ctx.define(dst, index);
+                ctx.using(src, index);
             }
 
             Instruction::Binary(dst, lhs, _, rhs) | Instruction::Index(dst, lhs, rhs) => {
-                ctx.defs.insert(*dst, index);
-                update(lhs);
-                update(rhs);
+                ctx.define(dst, index);
+                ctx.using(lhs, index);
+                ctx.using(rhs, index);
             }
             Instruction::Call(dst, src, args) | Instruction::Method(dst, src, _, args) => {
-                ctx.defs.insert(*dst, index);
-                update(src);
+                ctx.define(dst, index);
+                ctx.using(src, index);
                 for arg in args {
-                    update(arg);
+                    ctx.using(arg, index);
                 }
             }
             Instruction::List(dst, args) | Instruction::Tuple(dst, args) => {
-                ctx.defs.insert(*dst, index);
+                ctx.define(dst, index);
                 for arg in args {
-                    update(arg);
+                    ctx.using(arg, index);
                 }
             }
             Instruction::Group(dst, _)
             | Instruction::Function(dst, _)
             | Instruction::Load(dst, _) => {
-                ctx.defs.insert(*dst, index);
+                ctx.define(dst, index);
             }
         }
     }
 }
 
 impl Terminator {
-    fn dn(&self, index: usize, ctx: &mut Context) {
-        let mut update = |var: &Var| {
-            match ctx.uses.entry(*var) {
-                Entry::Occupied(mut e) => {
-                    let last = max(index, *e.get());
-                    e.insert(last);
-                }
-                Entry::Vacant(e) => {
-                    e.insert(index);
-                }
-            };
-        };
-        match self {
-            Terminator::Branch(var, _, _) | Terminator::Return(var) => {
-                update(var);
+    fn du(
+        &self,
+        index: usize,
+        ctx: &mut Context,
+        anchors: &mut HashMap<Label, usize>,
+        loops: &mut Vec<(usize, usize)>,
+    ) {
+        let mut extend = |label: &Label| {
+            if let Some(start) = anchors.get(label) {
+                loops.push((*start, index));
             }
-            Terminator::Jump(_) => {}
+        };
+
+        match self {
+            Terminator::Branch(var, yes, no) => {
+                ctx.using(var, index);
+                extend(yes);
+                extend(no);
+            }
+            Terminator::Return(var) => {
+                ctx.using(var, index);
+            }
+            Terminator::Jump(target) => {
+                extend(target);
+            }
         }
     }
 }
 
 impl Copy {
-    fn dn(&self, index: usize, ctx: &mut Context) {
-        let mut update = |var: &Var| {
-            match ctx.uses.entry(*var) {
-                Entry::Occupied(mut e) => {
-                    let last = max(index, *e.get());
-                    e.insert(last);
-                }
-                Entry::Vacant(e) => {
-                    e.insert(index);
-                }
-            };
-        };
-        ctx.defs.insert(self.0, index);
-        update(&self.1);
+    fn du(&self, index: usize, ctx: &mut Context) {
+        ctx.define(&self.0, index);
+        ctx.using(&self.1, index);
     }
 }
